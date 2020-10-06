bc5e7f08efc6840450fc5c4dfb66003baf7242ab
Fabirt <fandig27@gmail.com>
2020-10-06 12:24:39 -0500

    Customizing app theme

--- android/app/src/main/java/com/fabirt/kpopify/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/kpopify/MainActivity.kt
@@ -2,10 +2,19 @@ package com.fabirt.kpopify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureSystemUIForFullscreen()
+    }
+
+    private fun configureSystemUIForFullscreen() {
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+
+        window.decorView.systemUiVisibility = flags
     }
 }
