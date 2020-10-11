01e7d27358d008bec05c547c8bb893ec63fc61c7
Fabirt <fandig27@gmail.com>
2020-10-10 21:45:25 -0500

    Creating launch screen background

--- android/app/src/main/java/com/fabirt/kpopify/presentation/MainActivity.kt
+++ android/app/src/main/java/com/fabirt/kpopify/presentation/MainActivity.kt
@@ -8,6 +8,7 @@ import com.fabirt.kpopify.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
         configureSystemUIForFullscreen()
     }
