69d0ea42d4f80a312077cee938f9eb18d2bc582c
Fabirt <fandig27@gmail.com>
2020-10-11 18:19:19 -0500

    Creating MusicPlayerService. Adding dependencies

---
+++ android/app/src/main/java/com/fabirt/kpopify/core/services/MusicPlayerService.kt
@@ -0,0 +1,74 @@
+package com.fabirt.kpopify.core.services
+
+import android.app.PendingIntent
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MusicPlayerService : MediaBrowserServiceCompat() {
+
+    @Inject
+    lateinit var dataSourceFactory: DefaultDataSourceFactory
+
+    @Inject
+    lateinit var exoPlayer: SimpleExoPlayer
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    companion object {
+        private const val TAG = "MusicPlayerService"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val activityPendingIntent = packageManager.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        mediaSession = MediaSessionCompat(this, TAG).apply {
+            setSessionActivity(activityPendingIntent)
+            isActive = true
+        }
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession).also {
+            it.setPlayer(exoPlayer)
+        }
+
+        this.sessionToken = mediaSession.sessionToken
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+}
