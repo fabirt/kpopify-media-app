975ad18283ebe59d90cd233947fa4dd29c325d54
Fabirt <fandig27@gmail.com>
2020-10-12 01:20:42 -0500

    Creating main view model

--- android/app/src/main/java/com/fabirt/kpopify/di/AppModule.kt
+++ android/app/src/main/java/com/fabirt/kpopify/di/AppModule.kt
@@ -1,10 +1,13 @@
 package com.fabirt.kpopify.di
 
+import android.content.Context
+import com.fabirt.kpopify.core.exoplayer.MusicPlayerServiceConnection
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +17,10 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideMusicPlayerServiceConnection(
+        @ApplicationContext context: Context
+    ): MusicPlayerServiceConnection = MusicPlayerServiceConnection(context)
 }
---
+++ android/app/src/main/java/com/fabirt/kpopify/presentation/viewmodels/MainViewModel.kt
@@ -0,0 +1,89 @@
+package com.fabirt.kpopify.presentation.viewmodels
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.fabirt.kpopify.core.constants.K
+import com.fabirt.kpopify.core.exoplayer.MusicPlayerServiceConnection
+import com.fabirt.kpopify.core.exoplayer.isPlayEnabled
+import com.fabirt.kpopify.core.exoplayer.isPlaying
+import com.fabirt.kpopify.core.exoplayer.isPrepared
+import com.fabirt.kpopify.core.util.Resource
+import com.fabirt.kpopify.domain.model.Song
+
+class MainViewModel @ViewModelInject constructor(
+    private val serviceConnection: MusicPlayerServiceConnection
+) : ViewModel() {
+
+    private val _songs = MutableLiveData<Resource<List<Song>>>()
+    val song: LiveData<Resource<List<Song>>> get() = _songs
+
+    val isConnected = serviceConnection.isConnected
+    val networkError = serviceConnection.networkError
+    val currentPlayingSong = serviceConnection.currentPlayingSong
+    val playbackState = serviceConnection.playbackState
+
+    init {
+        _songs.postValue(Resource.Loading)
+        serviceConnection.subscribe(
+            K.MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {
+                override fun onChildrenLoaded(
+                    parentId: String,
+                    children: MutableList<MediaBrowserCompat.MediaItem>
+                ) {
+                    super.onChildrenLoaded(parentId, children)
+                    val items = children.map {
+                        Song(
+                            it.mediaId!!,
+                            it.description.title.toString(),
+                            it.description.subtitle.toString(),
+                            it.description.iconUri.toString(),
+                            it.description.mediaUri.toString(),
+                            "", ""
+                        )
+                    }
+                    _songs.postValue(Resource.Success(items))
+                }
+            })
+    }
+
+    fun skipToNextSong() {
+        serviceConnection.transportControls.skipToNext()
+    }
+
+    fun skipToPreviousSong() {
+        serviceConnection.transportControls.skipToPrevious()
+    }
+
+    fun seekTo(pos: Long) {
+        serviceConnection.transportControls.seekTo(pos)
+    }
+
+    fun playOrToggleSong(song: Song, toggle: Boolean = false) {
+        val isPrepared = playbackState.value?.isPrepared ?: false
+        val currentMediaId =
+            currentPlayingSong.value?.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)
+        if (isPrepared && song.mediaId == currentMediaId) {
+            playbackState.value?.let { state ->
+                when {
+                    state.isPlaying -> if (toggle) serviceConnection.transportControls.pause()
+                    state.isPlayEnabled -> serviceConnection.transportControls.play()
+                    else -> Unit
+                }
+            }
+        } else {
+            serviceConnection.transportControls.playFromMediaId(song.mediaId, null)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        serviceConnection.unsubscribe(
+            K.MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {})
+    }
+}
