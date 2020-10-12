ec4444e6aa89fba8a498ec3ad603b573dccea947
Fabirt <fandig27@gmail.com>
2020-10-12 10:18:42 -0500

    Removing onPlayerStateChanged callback

--- android/app/src/main/java/com/fabirt/kpopify/core/exoplayer/MusicPlayerEventListener.kt
+++ android/app/src/main/java/com/fabirt/kpopify/core/exoplayer/MusicPlayerEventListener.kt
@@ -8,12 +8,15 @@ import com.google.android.exoplayer2.Player
 class MusicPlayerEventListener(
     private val musicPlayerService: MusicPlayerService
 ) : Player.EventListener {
+    /*
     override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
         super.onPlayerStateChanged(playWhenReady, playbackState)
+        Stop foreground service
         if (playbackState == Player.STATE_READY && !playWhenReady) {
             musicPlayerService.stopForeground(false)
         }
     }
+     */
 
     override fun onPlayerError(error: ExoPlaybackException) {
         super.onPlayerError(error)
